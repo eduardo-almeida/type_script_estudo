a6bb5717cfe0ae95578f3ad78305b33a28af4887
Eduardo Almeida <eduardoeandroid@gmail.com>
2025-01-29 17:33:58 -0300

    Inicializando Estudo de typescript

---
+++ index.js
@@ -0,0 +1,6 @@
+var primeiroNome = "Eduardo";
+var sobrenome = "Almeida";
+function teste(name) {
+    console.log("Ola " + name);
+}
+teste(primeiroNome);
---
+++ index.ts
@@ -0,0 +1,9 @@
+const primeiroNome = "Eduardo";
+
+const sobrenome = "Almeida";
+
+function teste(name: string) {
+  console.log("Ola " + name);
+}
+
+teste(primeiroNome);
