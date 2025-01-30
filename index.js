64f99e1a730a03ca478a5568265e1dfad8c5c2a0
Eduardo Almeida <eduardoeandroid@gmail.com>
2025-01-30 15:39:47 -0300

    Aula 3 - completa

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const sobrenome = "Almeida";
 function teste(name) {
     console.log("Ola " + name);
 }
+function showNumbers(a, b, c) {
+    console.log("A: " + a);
+    if (b) {
+        console.log("B: " + b);
+    }
+    if (c) {
+        console.log("C: " + c);
+    }
+}
+showNumbers(1, c, 2);
 teste(primeiroNome);
--- index.ts
+++ index.ts
@@ -6,4 +6,16 @@ function teste(name: string) {
   console.log("Ola " + name);
 }
 
+function showNumbers(a: number, b?: number, c?: number) {
+  console.log("A: " + a);
+  if (b) {
+    console.log("B: " + b);
+  }
+  if (c) {
+    console.log("C: " + c);
+  }
+}
+
+showNumbers(1, c : 2);
+
 teste(primeiroNome);
